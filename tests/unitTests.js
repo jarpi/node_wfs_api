0db7d665bbec6662cc1f1b89a97dfd3c2a5c5744
jarpi <josep.arpi@gmail.com>
2015-03-23 16:21:50 +0100

    - Test travis failing test

--- tests/unitTests.js
+++ tests/unitTests.js
@@ -3,6 +3,7 @@ var wfsApp = require("../WfsApp.js");
 function unitTests() {}; 
 unitTests.LoadConfig = function(test) 
 {
+	test.ok(true==false, true, "Test failing"); 
 	var appLayer = new wfsApp(); 
 	test.ok(appLayer, {}, "Instance has params"); 
 	appLayer.LoadConfig(); 
